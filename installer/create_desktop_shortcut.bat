aef26660da2d401e735b5c66dec2f668fb319cd7
meizhong986 <148393222+meizhong986@users.noreply.github.com>
2025-08-31 17:06:16 +0100

    Installer: avoid PyInstaller EXE to reduce Defender false positives; desktop shortcut launches pythonw -m whisperjav.gui.whisperjav_gui; clean any existing EXE in post-install.

--- installer/create_desktop_shortcut.bat
+++ installer/create_desktop_shortcut.bat
@@ -3,10 +3,11 @@ echo Creating desktop shortcut...
 
 set SCRIPT_DIR=%~dp0
 set SHORTCUT_NAME=WhisperJAV.lnk
-set TARGET=%SCRIPT_DIR%WhisperJAV.exe
+set PYTHONW=%SCRIPT_DIR%pythonw.exe
+set TARGET_ARGS=-m whisperjav.gui.whisperjav_gui
 set ICON=%SCRIPT_DIR%whisperjav_icon.ico
 
-powershell -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\%SHORTCUT_NAME%'); $Shortcut.TargetPath = '%TARGET%'; $Shortcut.IconLocation = '%ICON%'; $Shortcut.Save()"
+powershell -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\%SHORTCUT_NAME%'); $Shortcut.TargetPath = '%PYTHONW%'; $Shortcut.Arguments = '%TARGET_ARGS%'; $Shortcut.IconLocation = '%ICON%'; $Shortcut.WorkingDirectory = '%SCRIPT_DIR%'; $Shortcut.Save()"
 
 if exist "%USERPROFILE%\Desktop\%SHORTCUT_NAME%" (
     echo Desktop shortcut created successfully!
