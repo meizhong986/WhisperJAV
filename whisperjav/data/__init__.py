645dd24fea2336eb44b8526c9538f90925b3e057
meizhong986 <148393222+meizhong986@users.noreply.github.com>
2025-12-15 01:24:10 +0000

    added caching of hallucintion fileters and regexp

---
+++ whisperjav/data/__init__.py
@@ -0,0 +1,7 @@
+# whisperjav/data/__init__.py
+"""
+Bundled data files for WhisperJAV.
+
+This package contains static data files that are bundled with the application
+for offline use and faster startup times.
+"""
